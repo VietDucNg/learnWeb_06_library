615e0d629d3289888ab0654621bd2c33625d01a7
Viet <vietducnguyen.fit@gmail.com>
2026-01-02 15:14:37 +0100

    feat: add delAllConfirm modialog

--- js/removeAllBtn.js
+++ js/removeAllBtn.js
@@ -1,5 +1,10 @@
 import { delAllBook } from './main.js';
 
 const delAllBtn = document.querySelector('.delAll-btn');
+const delAllDialog = document.querySelector('.delAll-dialog');
 
-delAllBtn.addEventListener('click', delAllBook);
+delAllBtn.addEventListener('click', ()=> delAllDialog.showModal());
+
+delAllDialog.addEventListener('close', ()=>{
+    if (delAllDialog.returnValue === 'confirm') delAllBook();
+});
